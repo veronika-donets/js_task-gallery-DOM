9228a243f08b74bccdf0d798b74811f624c2111b
Veronika D <donets.veronika@yahoo.com>
2019-10-19 18:22:37 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,9 @@
 'use strict';
 
-console.log('hello mate academy!');
+const item = document.getElementById('thumbs');
+
+item.addEventListener('click', (targetEvent) => {
+  targetEvent.preventDefault();
+  const largeImage = document.getElementById('largeImg');
+  largeImage.src = targetEvent.target.closest('a').href;
+});
