dda99ae470f7ab4137775c75f63f0333cba14166
Veronika D <donets.veronika@yahoo.com>
2019-10-21 15:26:52 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const item = document.getElementById('thumbs');
+const largeImage = document.getElementById('largeImg');
 
 item.addEventListener('click', (targetEvent) => {
   targetEvent.preventDefault();
-  const largeImage = document.getElementById('largeImg');
+
   largeImage.src = targetEvent.target.closest('a').href;
+  largeImage.alt = targetEvent.target.closest('a').title;
 });
